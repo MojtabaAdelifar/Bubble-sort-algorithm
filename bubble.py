08259ec4eac8fc36daf68d0d2db3f62961108bf7
MojtabaAdelifar <93451112+MojtabaAdelifar@users.noreply.github.com>
2022-12-22 07:25:38 -0800

    Update bubble.py

--- bubble.py
+++ bubble.py
@@ -1,9 +1,12 @@
 
 # bubble sort algorithm
 def bubbleSort(array):
+    # loops for entire list
     for i in range(len(array)):
+        # loops through array to compare to finde the position for the greates element in unsorted part
         for j in range(0, len(array) - i - 1):
             if array[j] > array[j + 1]:
+                # swap the elements
                 temp = array[j]
                 array[j] = array[j + 1]
                 array[j + 1] = temp
