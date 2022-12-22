1f7127ad122ceda36c07abb9e8532d6c945d027a
MojtabaAdelifar <93451112+MojtabaAdelifar@users.noreply.github.com>
2022-12-22 07:12:40 -0800

    Add files via upload

---
+++ bubble.py
@@ -0,0 +1,22 @@
+
+# bubble sort algorithm
+def bubbleSort(array):
+    for i in range(len(array)):
+        for j in range(0, len(array) - i - 1):
+            if array[j] > array[j + 1]:
+                temp = array[j]
+                array[j] = array[j + 1]
+                array[j + 1] = temp
+
+    return array
+
+my_array = [-1, -3, 0 ,10, 5, 7]
+
+print("Unsorted array:", end=' ')
+for i in range(0, len(my_array)):
+    print(my_array[i], end= ' ')
+
+sorted = bubbleSort(my_array)
+print("\nSorted array:", end= ' ')
+for i in sorted:
+    print(i, end=' ')
